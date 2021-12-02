9f0fe066824e2c6f4a0495b06d71083ab2ed2456
WarriorsSami <sbarbutdica@gmail.com>
2021-12-02 12:38:31 +0200

    ops: configure sdl lib

---
+++ Procedural/cmake-build-debug/CMakeFiles/Procedural.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Procedural.dir/main.c.o"
+  "Procedural"
+  "Procedural.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Procedural.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Procedural/cmake-build-debug/CMakeFiles/Procedural.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Procedural.
+# This may be replaced when dependencies are built.
